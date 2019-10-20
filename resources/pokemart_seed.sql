2c6aa7e1f0dbce5d772d036dec0b2f2c90d5fb9d
Matthew <matthew_carpenter@icloud.com>
2019-10-20 12:08:07 -0700

    added logic for seed and tables.

--- resources/pokemart_seed.sql
+++ resources/pokemart_seed.sql
@@ -7,7 +7,7 @@ CREATE DATABASE pokemartDB;
 -- Now select this database so that we may start to to add tables.
 USE pokemartDB;
 
--- Create first of two tables (shop_inventory_table) and (pokemon_trade_table);
+-- Create first of two tables (shop_inventory_table) and (pokemon_bank_table);
 
 -- shop_inventory_table
 -- csv schema (item_name, item_description, item_type, item_cost, item_inventory_level)
@@ -15,7 +15,7 @@ CREATE TABLE shop_inventory_table (
     -- set the ID of items to a number schema.
     item_id INT NOT NULL AUTO_INCREMENT,
     item_name VARCHAR(64) NOT NULL,
-    item_description (256),
+    item_description VARCHAR(256),
     item_type VARCHAR(32),
     item_cost INT NOT NULL,
     item_inventory_level INT NOT NULL,
@@ -23,12 +23,23 @@ CREATE TABLE shop_inventory_table (
     PRIMARY KEY (item_id)
 );
 
--- pokemon_trade_table 
--- schema (pokemon_name, pokemon_type, pokemon_level, pokemon_star)
-CREATE TABLE pokemon_trade_table (
+-- pokemon_bank_table 
+-- schema (pokemon_number *int*, pokemon_name *string*, pokemon_type *string*, pokemon_level *int*, pokemon_is_shiny *bool*)
+CREATE TABLE pokemon_bank_table (
     pokemon_id INT NOT NULL AUTO_INCREMENT,
+    pokemon_number INT NOT NULL,
     pokemon_name VARCHAR(64) NOT NULL,
     pokemon_type VARCHAR(32) NOT NULL,
     pokemon_level INT NOT NULL,
     pokemon_is_shiny BOOL DEFAULT "false"
+    -- other stats would go here if we were tracking Pokemon Stats
+    -- hp
+    -- str
+    -- moves
+    -- ability power
+    -- experience
+
+    -- assign the primary key to the item_id
+    -- the item ID is NOT the pokemon number. That is determined by its original name.
+    PRIMARY KEY (pokemon_id)
 );
